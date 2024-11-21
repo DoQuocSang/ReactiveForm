a73d713d7adcc128189067d961c550026c9c4a66
DoQuocSang <quocsang316@gmail.com>
2024-11-21 17:27:48 +0700

    Bug v1: Error that cause lost index of formgroup

--- src/src/app/custom-form/custom-form.component.ts
+++ src/src/app/custom-form/custom-form.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-custom-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './custom-form.component.html',
 })
 export class CustomFormComponent {
-  name = new FormControl('');
+  private formBuilder = inject(FormBuilder);
+
+  productForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    brand: [''],
+    price: ['0', Validators.required],
+    variants: this.formBuilder.array([
+      this.formBuilder.group({
+        color: [''],
+        size: ['0'],
+        quantity: ['0'],
+      }),
+    ]),
+  });
+
+  private _variants = this.productForm.get('variants') as FormArray;
+
+  get variants() {
+    return this._variants;
+  }
+
+  deleteAllVariants() {
+    this._variants = this.formBuilder.array([]);
+  }
+
+  deleteVariant(index: number) {
+    this._variants.removeAt(index);
+  }
+
+  addVariant() {
+    this._variants.push(
+      this.formBuilder.group({
+        color: ['blue'],
+        size: ['30'],
+        quantity: ['0'],
+      })
+    );
+
+    console.log(this._variants.value);
+  }
+
+  onSubmit() {
+    console.warn(this.productForm.value);
+  }
 }
