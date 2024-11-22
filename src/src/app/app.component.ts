0d6baab3789d4a1418b656e3c85e97d38b5a93f4
DoQuocSang <quocsang316@gmail.com>
2024-11-22 17:35:01 +0700

    Split UI to components and using component store

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { CustomFormComponent } from './custom-form/custom-form.component';
+import {
+  CustomFormComponent,
+} from './components/custom-form/custom-form.component';
+import {
+  ProductVariantFormComponent,
+} from './components/product-variant-form/product-variant-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CustomFormComponent],
+  imports: [RouterOutlet, CustomFormComponent, ProductVariantFormComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
